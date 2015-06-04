e34df18994cf89bc263b20dfeab6466ccbe9b462
Theodore Alexandrov <theodore@uni-bremen.de>
2015-06-03 23:00:40 -0400

    G App width increased

--- chromeapp-background.js
+++ chromeapp-background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     'bounds': {
-      'width': 400,
+      'width': 1000,
       'height': 500
     }
   });
