a9a3ac2b67664fe33d38a1acd6681e027527fbb3
Ivan Protsyuk <iprotsyuk@gmail.com>
2016-04-14 18:22:24 +0200

    Requirizing spotlabel3d

--- js/SpotLabel3D.js
+++ js/SpotLabel3D.js
@@ -1,65 +1,72 @@
-'use strict';
+define([
+    'spotlabelbase'
+],
+function(SpotLabelBase) {
+    'use strict';
 
-function SpotLabel3D(group, scene) {
-    SpotLabelBase.apply(this);
-    this._group = group;
-    this._scene = scene;
-    this._view = null;
-    this._raycastPromise = null;
-    this._div = null;
-    this._changed = false;
-}
-
-SpotLabel3D.prototype = Object.create(SpotLabelBase.prototype, asProps({
-    showFor: function(pageX, pageY) {
-        if (this._raycastPromise) {
-            this._raycastPromise.cancel();
-            this._raycastPromise = null;
-        }
-        this._view = this._group.findView(pageX, pageY);
-        this._spot = null;
-        this._changed = true;
-        if (this._view) {
-            var raycaster = new THREE.Raycaster();
-            this._view.setupRaycaster(raycaster, pageX, pageY);
-
-            this._raycastPromise = this._scene.raycast(raycaster);
-            if (this._raycastPromise) {
-                this._raycastPromise.then(this._onRaycastComplete.bind(this));
-            } else {
-                this._view = null;
-            }
-        }
-        this._group.requestAnimationFrame();
-    },
-
-    _onRaycastComplete: function(spot) {
+    function SpotLabel3D(group, scene) {
+        SpotLabelBase.apply(this);
+        this._group = group;
+        this._scene = scene;
+        this._view = null;
         this._raycastPromise = null;
-        this._spot = spot;
-        this._changed = true;
-        this._group.requestAnimationFrame();
-    },
+        this._div = null;
+        this._changed = false;
+    }
 
-    update: function() {
-        if (this._changed) {
-            if (this.div) {
-                this.removeDiv();
+    SpotLabel3D.prototype = Object.create(SpotLabelBase.prototype, asProps({
+        showFor: function(pageX, pageY) {
+            if (this._raycastPromise) {
+                this._raycastPromise.cancel();
+                this._raycastPromise = null;
             }
-            if (this._view && this._spot) {
-                this.createDiv('SpotLabel3D');
-                this.textContent = this._spot.name;
-                this._view.div.appendChild(this.div);
-            }
-            this._changed = false;
-        }
+            this._view = this._group.findView(pageX, pageY);
+            this._spot = null;
+            this._changed = true;
+            if (this._view) {
+                var raycaster = new THREE.Raycaster();
+                this._view.setupRaycaster(raycaster, pageX, pageY);
 
-        if (this._view && this._spot && this._div) {
-            var position = this._scene.spotToWorld(this._spot);
-            if (position) {
-                var point2D = this._view.projectPosition(position);
-                this._div.style.left = point2D.x + 'px'
-                this._div.style.top = point2D.y + 'px'
+                this._raycastPromise = this._scene.raycast(raycaster);
+                if (this._raycastPromise) {
+                    this._raycastPromise.then(this._onRaycastComplete.bind(this));
+                } else {
+                    this._view = null;
+                }
             }
-        }
-    },
-}));
+            this._group.requestAnimationFrame();
+        },
+
+        _onRaycastComplete: function(spot) {
+            this._raycastPromise = null;
+            this._spot = spot;
+            this._changed = true;
+            this._group.requestAnimationFrame();
+        },
+
+        update: function() {
+            if (this._changed) {
+                if (this.div) {
+                    this.removeDiv();
+                }
+                if (this._view && this._spot) {
+                    this.createDiv('SpotLabel3D');
+                    this.textContent = this._spot.name;
+                    this._view.div.appendChild(this.div);
+                }
+                this._changed = false;
+            }
+
+            if (this._view && this._spot && this._div) {
+                var position = this._scene.spotToWorld(this._spot);
+                if (position) {
+                    var point2D = this._view.projectPosition(position);
+                    this._div.style.left = point2D.x + 'px'
+                    this._div.style.top = point2D.y + 'px'
+                }
+            }
+        },
+    }));
+
+    return SpotLabel3D;
+});
