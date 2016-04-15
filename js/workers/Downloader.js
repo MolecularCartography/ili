99e32e05f87b025aa62037cb30cdc416f3b93a90
Ivan Protsyuk <iprotsyuk@gmail.com>
2016-04-15 16:01:59 +0200

    Requirized some workers

--- js/workers/Downloader.js
+++ js/workers/Downloader.js
@@ -1,114 +1,125 @@
 'use strict';
 
-onmessage = function(e) {
-    var fileNames = e.data;
-    var downloader = new Downloader();
+define([],
+function() {
+    onmessage = function (e) {
+        var fileNames = e.data;
+        var downloader = new Downloader();
 
-    try {
-        for (var i = 0; i < fileNames.length; i++) {
-            downloader.add(fileNames[i]);
+        try {
+            for (var i = 0; i < fileNames.length; i++) {
+                downloader.add(fileNames[i]);
+            }
+
+            downloader.start();
+        } catch (e) {
+            postMessage({
+                status: 'failed',
+                message: e.toString(),
+            });
         }
+    };
 
-        downloader.start();
-    } catch (e) {
-        postMessage({
-            status: 'failed',
-            message: e.toString(),
-        });
+    function Downloader() {
+        this.items = [];
+        this.completed = 0;
+        this.failed = false;
     }
-};
 
-function Downloader() {
-    this.items = [];
-    this.completed = 0;
-    this.failed = false;
-}
+    Downloader.DATA_PATH = '../../data';
 
-Downloader.DATA_PATH = '../../data';
+    Downloader.prototype = {
+        add: function (fileName) {
+            var item = new Downloader.Item(fileName);
+            item.request.onprogress = this.onProgress.bind(this, item);
+            item.request.onerror = this.onError.bind(this, item);
+            item.request.onload = this.onLoad.bind(this, item);
+            this.items.push(item);
+        },
 
-Downloader.prototype = {
-    add: function(fileName) {
-        var item = new Downloader.Item(fileName);
-        item.request.onprogress = this.onProgress.bind(this, item);
-        item.request.onerror = this.onError.bind(this, item);
-        item.request.onload = this.onLoad.bind(this, item);
-        this.items.push(item);
-    },
+        start: function () {
+            for (var i = 0; i < this.items.length; i++) {
+                this.items[i].request.send();
+            }
+        },
 
-    start: function() {
-        for (var i = 0; i < this.items.length; i++) {
-            this.items[i].request.send();
-        }
-    },
+        onProgress: function (item, event) {
+            item.total = event.total;
+            item.loaded = event.loaded;
 
-    onProgress: function(item, event) {
-        item.total = event.total;
-        item.loaded = event.loaded;
+            var total = this._sum(function (item) {
+                return item.total;
+            });
+            var loaded = this._sum(function (item) {
+                return item.loaded;
+            });
 
-        var total = this._sum(function(item) { return item.total; });
-        var loaded = this._sum(function(item) { return item.loaded; });
+            var progress = loaded > 0 ? loaded + (total > 0 ? ' of ' + total : '') : '';
 
-        var progress = loaded > 0 ? loaded + (total > 0 ? ' of ' + total : '') : '';
-
-        postMessage({
+            postMessage({
                 status: 'working',
                 message: 'Downloading' + (progress ? ' ' + progress : '...'),
+            });
+        },
+
+        onError: function (item, event) {
+            if (this.failed) return;
+            this.failed = true;
+
+            console.info('Error loading ' + item.fileName + ': ' + item.request.statusText, event);
+            this.items.forEach(function (item) {
+                item.request.abort();
+            });
+
+            postMessage({
+                status: 'failed',
+                message: 'Can not download ' + item.fileName + '. See log for details.',
+            });
+        },
+
+        onLoad: function (item, event) {
+            if (item.request.status >= 300) {
+                this.onError(item, event);
+                return;
+            }
+            this.completed++;
+            if (this.completed != this.items.length) return;
+
+            postMessage({
+                status: 'completed',
+                items: this.items.map(function (item) {
+                    return {
+                        blob: item.request.response,
+                        fileName: item.fileName
+                    };
+                }),
+            });
+        },
+
+        _sum: function (f) {
+            var r = 0;
+            for (var i = 0; i < this.items.length; i++) {
+                r += f(this.items[i]);
+            }
+            return r;
+        },
+    };
+
+    Downloader.Item = function (fileName) {
+        var path = Downloader.DATA_PATH.split('/');
+        fileName.split('/').forEach(function (chunk) {
+            if (!/^\w[\w\.-]*$/.test(chunk)) {
+                throw 'File "' + fileName + '" can\'t be downloaded';
+            }
+            path.push(chunk);
         });
-    },
+        this.fileName = fileName;
+        this.request = new XMLHttpRequest();
+        this.request.open('GET', path.join('/'), true);
+        this.request.responseType = 'blob';
+        this.total = NaN;
+        this.loaded = 0;
+    };
 
-    onError: function(item, event) {
-        if (this.failed) return;
-        this.failed = true;
-
-        console.info('Error loading ' + item.fileName + ': ' + item.request.statusText, event);
-        this.items.forEach(function(item) { item.request.abort(); });
-
-        postMessage({
-            status: 'failed',
-            message: 'Can not download ' + item.fileName + '. See log for details.',
-        });
-    },
-
-    onLoad: function(item, event) {
-        if (item.request.status >= 300) {
-            this.onError(item, event);
-            return;
-        }
-        this.completed++;
-        if (this.completed != this.items.length) return;
-
-        postMessage({
-            status: 'completed',
-            items: this.items.map(function(item) {
-                return {
-                    blob: item.request.response,
-                    fileName: item.fileName
-                };
-            }),
-        });
-    },
-
-    _sum: function(f) {
-        var r = 0;
-        for (var i = 0; i < this.items.length; i++) {
-            r += f(this.items[i]);
-        }
-        return r;
-    },
-};
-
-Downloader.Item = function(fileName) {
-    var path = Downloader.DATA_PATH.split('/');
-    fileName.split('/').forEach(function(chunk) {
-        if (!/^\w[\w\.-]*$/.test(chunk)) {
-            throw 'File "' + fileName + '" can\'t be downloaded';
-        }
-        path.push(chunk);
-    });
-    this.fileName = fileName;
-    this.request = new XMLHttpRequest();
-    this.request.open('GET', path.join('/'), true);
-    this.request.responseType = 'blob';
-    this.total = NaN;
-    this.loaded = 0;
-};
+    return onmessage;
+});
