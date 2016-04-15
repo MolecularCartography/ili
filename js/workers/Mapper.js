c8f5a838febaaea6a6be79c1fbbf26351c19c42e
Antonio Gonzalez <antgonza@gmail.com>
2016-04-15 06:23:57 -0600

    init commit

--- js/workers/Mapper.js
+++ js/workers/Mapper.js
@@ -1,66 +1,73 @@
-/**
- * Web worker what builds association between points on the mesh (only used for
- * MODE_3D) and spots. Having this accosiation prebuilt recoloring the mesh for
- * any specific map related to the same spots could be done much faster.
- */
+'use strict';
 
-onmessage = function(e) {
-    var positions = e.data.verteces;
-    var spots = e.data.spots;
+define([],
+    function () {
+    /**
+     * Web worker what builds association between points on the mesh (only used for
+     * MODE_3D) and spots. Having this accosiation prebuilt recoloring the mesh for
+     * any specific map related to the same spots could be done much faster.
+     */
 
-    var pointCount = (positions.length / 3) | 0;
-    var closestSpotIndeces = new Int32Array(pointCount);
-    var closestSpotDistances = new Float32Array(pointCount);
-    var progress = -1;
-    var nextChunk = 0;
-    var highlightedVerteces = 0;
+    onmessage = function(e) {
+        var positions = e.data.verteces;
+        var spots = e.data.spots;
 
-    for (var i = 0; i < pointCount; i++) {
-        while (i >= nextChunk) {
-            progress++;
-            nextChunk = Math.ceil((progress + 1) * pointCount / 100);
-            postMessage({
-                status: 'working',
-                message: 'Mapping: ' + progress + '%',
-            });
-        }
-        var positionOffset = i * 3;
-        var x = positions[positionOffset + 0];
-        var y = positions[positionOffset + 1];
-        var z = positions[positionOffset + 2];
+        var pointCount = (positions.length / 3) | 0;
+        var closestSpotIndeces = new Int32Array(pointCount);
+        var closestSpotDistances = new Float32Array(pointCount);
+        var progress = -1;
+        var nextChunk = 0;
+        var highlightedVerteces = 0;
 
-        var closestSpotIndex = -1;
-        var closesSpotSquareDistance;
+        for (var i = 0; i < pointCount; i++) {
+            while (i >= nextChunk) {
+                progress++;
+                nextChunk = Math.ceil((progress + 1) * pointCount / 100);
+                postMessage({
+                    status: 'working',
+                    message: 'Mapping: ' + progress + '%',
+                });
+            }
+            var positionOffset = i * 3;
+            var x = positions[positionOffset + 0];
+            var y = positions[positionOffset + 1];
+            var z = positions[positionOffset + 2];
 
-        for (var j = 0; j < spots.length; j++) {
-            var spot = spots[j];
+            var closestSpotIndex = -1;
+            var closesSpotSquareDistance;
 
-            var dx = spot.x - x;
-            var dy = spot.y - y;
-            var dz = spot.z - z;
-            var rsq = dx * dx + dy * dy + dz * dz;
+            for (var j = 0; j < spots.length; j++) {
+                var spot = spots[j];
 
-            if (rsq > spot.r * spot.r) continue;
+                var dx = spot.x - x;
+                var dy = spot.y - y;
+                var dz = spot.z - z;
+                var rsq = dx * dx + dy * dy + dz * dz;
 
-            if (closestSpotIndex < 0 || rsq < closesSpotSquareDistance) {
-                closesSpotSquareDistance = rsq;
-                closestSpotIndex = j;
+                if (rsq > spot.r * spot.r) continue;
+
+                if (closestSpotIndex < 0 || rsq < closesSpotSquareDistance) {
+                    closesSpotSquareDistance = rsq;
+                    closestSpotIndex = j;
+                }
+            }
+
+            closestSpotIndeces[i] = closestSpotIndex;
+            if (closestSpotIndex >= 0) {
+                closestSpotDistances[i] = Math.sqrt(closesSpotSquareDistance) /
+                                          spots[closestSpotIndex].r;
+                highlightedVerteces++;
+            } else {
+                closestSpotDistances[i] = 1.0;
             }
         }
 
-        closestSpotIndeces[i] = closestSpotIndex;
-        if (closestSpotIndex >= 0) {
-            closestSpotDistances[i] = Math.sqrt(closesSpotSquareDistance) /
-                                      spots[closestSpotIndex].r;
-            highlightedVerteces++;
-        } else {
-            closestSpotDistances[i] = 1.0;
-        }
-    }
+        postMessage({
+            status: 'completed',
+            closestSpotIndeces: closestSpotIndeces,
+            closestSpotDistances: closestSpotDistances,
+        });
+    };
 
-    postMessage({
-        status: 'completed',
-        closestSpotIndeces: closestSpotIndeces,
-        closestSpotDistances: closestSpotDistances,
-    });
-};
+    return onmessage;
+});
--- js/workers/MeshLoader.js
+++ js/workers/MeshLoader.js
@@ -2,42 +2,48 @@
  * Web Worker. Loads a mesh from STL file.
  */
 
-importScripts('../lib/three.min.js', '../lib/STLLoader.js');
 
-onmessage = function(e) {
-    var blob = e.data;
-    var reader = new FileReaderSync();
-    readContents(reader.readAsArrayBuffer(blob));
-};
+define([
+    'three'
+],
+function (STLLoader) {
+    onmessage = function(e) {
+        var blob = e.data;
+        var reader = new FileReaderSync();
+        readContents(reader.readAsArrayBuffer(blob));
+    };
 
-function readContents(contents) {
-    try {
-        var geometry = new THREE.STLLoader().parse(contents);
-    } catch (e) {
-        console.info('Failure parsing STL file', e);
+    function readContents(contents) {
+        try {
+            var geometry = new THREE.STLLoader().parse(contents);
+        } catch (e) {
+            console.info('Failure parsing STL file', e);
+            postMessage({
+                    status: 'failed',
+                    message: 'Can not parse STL file. See log for details.',
+            });
+        }
+
+        // TODO: This only works with binary file format. Handle text format.
+        var attributes = {};
+        for (var name in geometry.attributes) {
+            attributes[name] = {
+                array: geometry.attributes[name].array,
+                itemSize: geometry.attributes[name].itemSize
+            };
+        }
+
         postMessage({
-                status: 'failed',
-                message: 'Can not parse STL file. See log for details.',
+                status: 'completed',
+                attributes: attributes,
         });
     }
 
-    // TODO: This only works with binary file format. Handle text format.
-    var attributes = {};
-    for (var name in geometry.attributes) {
-        attributes[name] = {
-            array: geometry.attributes[name].array,
-            itemSize: geometry.attributes[name].itemSize
-        };
+    function cloneBufferAttribute(origin) {
+        var array = new Float32Array(origin.array.length);
+        array.set(origin.array);
+        return new THREE.BufferAttribute(array, origin.itemSize);
     }
 
-    postMessage({
-            status: 'completed',
-            attributes: attributes,
-    });
-}
-
-function cloneBufferAttribute(origin) {
-    var array = new Float32Array(origin.array.length);
-    array.set(origin.array);
-    return new THREE.BufferAttribute(array, origin.itemSize);
-}
+    return { 'onmessage': onmessage, 'cloneBufferAttribute': cloneBufferAttribute, 'readContents': readContents }
+});
