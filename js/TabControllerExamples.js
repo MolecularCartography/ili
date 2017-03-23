480d8ce31c6d67988f56e5e894b5ea58b5ea751f
Ivan Protsyuk <iprotsyuk@gmail.com>
2017-03-23 17:31:48 +0100

    Implemented examples using cartographical snapshots

--- js/TabControllerExamples.js
+++ js/TabControllerExamples.js
@@ -9,67 +9,32 @@ function (TabControllerBase, Workspace) {
         var title = 'Examples';
         TabControllerBase.call(this, container, title, description, workspace);
 
-        var openExample = function () {
-            workspace.download(this.files);
-            this.adjustView();
-        };
         var items = [
             {
                 name: '3D-MASSOMICS meeting mockup',
-                files: ['3dmassomics/bg.png', '3dmassomics/intensities.csv'],
-                adjustView: function () {
-                    workspace.colorMapId = 'VIRIDIS';
-                }
+                files: ['3dmassomics/bg.png', '3dmassomics/intensities.csv', '3dmassomics/example.json']
             },
             {
                 name: 'Stingless bee',
-                files: ['bee/model.stl', 'bee/intensities.csv'],
-                adjustView: function () {
-                    workspace.scene3d.adjustment.alpha = -90;
-                    workspace.scene3d.adjustment.beta = 10;
-                    workspace.scene3d.adjustment.gamma = 0;
-                    workspace.scene3d.adjustment.x = 0;
-                    workspace.scene3d.adjustment.y = 0;
-                    workspace.scene3d.adjustment.z = 0;
-
-                    workspace.scaleId = Workspace.Scale.LOG.id;
-                    workspace.colorMapId = 'VIRIDIS';
-                }
+                files: ['bee/model.stl', 'bee/intensities.csv', , 'bee/example.json']
             },
             {
                 name: 'Diseased coral',
-                files: ['coral/bg.png', 'coral/intensities.csv'],
-                adjustView: function () {
-                    workspace.colorMapId = 'VIRIDIS';
-                }
+                files: ['coral/bg.png', 'coral/intensities.csv', 'coral/example.json']
             },
             {
                 name: 'Cyanobacteria natural products',
-                files: ['cyano/bg.png', 'cyano/intensities.csv'],
-                adjustView: function () {
-                    workspace.scaleId = Workspace.Scale.LOG.id;
-                    workspace.colorMapId = 'JET';
-                }
+                files: ['cyano/bg.png', 'cyano/intensities.csv', 'cyano/example.json']
             },
             {
                 name: 'Human skin metabolome',
-                files: ['human/man.stl', 'human/man_LCMS_small.csv'],
-                adjustView: function () {
-                    workspace.colorMapId = 'VIRIDIS';
-
-                    workspace.scene3d.adjustment.alpha = -90;
-                    workspace.scene3d.adjustment.beta = 0;
-                    workspace.scene3d.adjustment.gamma = -45;
-                    workspace.scene3d.adjustment.x = 0;
-                    workspace.scene3d.adjustment.y = -13;
-                    workspace.scene3d.adjustment.z = 0;
-                }
+                files: ['human/man.stl', 'human/man_LCMS_small.csv', 'human/example.json']
             }
         ];
 
         this.addHintBlock('Select any of the examples below');
         items.forEach(function (item) {
-            item[item.name] = openExample.bind(item);
+            item[item.name] = workspace.download.bind(workspace, item.files);
             this.addAction(item.name, item[item.name]);
         }.bind(this));
 
