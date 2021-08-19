8984b59d52577e80002c233b0c62e84b7cf6bb0d
vviolka <wiolka24324@ya.ru>
2021-08-19 15:43:40 +0300

    Transfer function control: density based ticks

--- js/common/ui/ControlsGrid.js
+++ js/common/ui/ControlsGrid.js
@@ -379,9 +379,8 @@ function(bs_colorpicker, bs_select, bs_slider, bs_spinbox, transferFunctionContr
                 var controlId = controlIds['control-id'];
 
                 var subContainerId = 'sub-' + controlId;
-                var subSubContainerId = 'sub-sub-' + controlId
                 var layout = '<div class="row"><div class="col-xs-12"><div class="panel-group">';
-                layout += '<div class="panel panel-default" id="' + subSubContainerId + '" data-toggle="collapse" data-target="#'
+                layout += '<div class="panel panel-default" data-toggle="collapse" data-target="#'
                     + subContainerId + '"><div id="' + controlId + '" class="panel-heading">';
                 layout += '<div class="panel-title btn-text">' + '<span id="arrow-' + controlId
                     + '" class="indicator glyphicon glyphicon-chevron-down pull-left"></span>&nbsp;'
@@ -394,7 +393,7 @@ function(bs_colorpicker, bs_select, bs_slider, bs_spinbox, transferFunctionContr
                 this._params[this._toKey(name)] = result;
 
                 var collapseIndicator = this._$container.find('#arrow-' + controlId);
-                this._$container.find('#' + subSubContainerId).on('click', function () {
+                this._$container.find('div.panel.panel-default').on('click', function () {
                     collapseIndicator.toggleClass('glyphicon-chevron-down');
                     collapseIndicator.toggleClass('glyphicon-chevron-up');
                 });
